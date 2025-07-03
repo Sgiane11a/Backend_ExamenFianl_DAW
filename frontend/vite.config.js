4100506df11c077bac5d7e28b24df0b534b108eb
Sgiane11a <steyci.cordova@tecsup.edu.pe>
2025-07-02 21:27:49 -0500

    Backend

--- frontend/vite.config.js
+++
@@ -1,8 +0,0 @@
-// Contenido NUEVO (CORRECTO)
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // <--- Esta es la línea corregida
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
